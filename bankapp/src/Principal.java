44b7b1a0e8e58622f531b6c2e4eb1977df20e470
odnxmrx <armandopinedag@outlook.com>
2025-05-11 22:08:25 -0600

    add bank app project

--- bankapp/src/Principal.java
+++ bankapp/src/Principal.java
@@ -1,2 +1,58 @@
+import java.util.Scanner;
+
 public class Principal {
+    public static void main(String[] args) {
+        // Inicializamos variables
+        String clientName = "Armando Gama";
+        String accountTipe = "Regular";
+        double balance = 2610.50;
+        int selectedOption = 0;
+        String optionsMenu = """
+                ** Please, enter the number of the desired transaction: **
+                1 - Balance
+                2 - Withdrawal
+                3 - Deposit
+                9 - Exit
+                """;
+
+        System.out.println("-------------\n");
+        System.out.println("Client name: " + clientName);
+        System.out.println("Your account type: " + accountTipe);
+        System.out.println("Your current balance is: $" + balance);
+        System.out.println("\n-------------");
+
+        Scanner keyboardInput = new Scanner(System.in);
+        while (selectedOption != 9){
+            System.out.println(optionsMenu);
+            selectedOption = keyboardInput.nextInt();
+
+            // Navigate to selected option
+            switch (selectedOption){
+                case 1:
+                    System.out.println("Your current balance is: $" + balance);
+                    break;
+                case 2:
+                    System.out.println("Amount to withdraw?");
+                    double desiredAmountToWithdraw = keyboardInput.nextDouble();
+                    if (balance < desiredAmountToWithdraw){
+                        System.out.println("Sorry, your balance is not enough.");
+                    } else {
+                        balance -= desiredAmountToWithdraw;
+                        System.out.println("Your updated balance is: $" + balance);
+                    }
+                    break;
+                case 3:
+                    System.out.println("Amount to deposit?");
+                    double amountToDeposit = keyboardInput.nextDouble();
+                    balance += amountToDeposit;
+                    System.out.println("Your updated balance is: $" + balance);
+                    break;
+                case 9:
+                    System.out.println("Exiting now. Thank you for trusting in us.");
+                    break;
+                default:
+                    System.out.println("Option is not valid. Please, enter a valid number.");
+            }
+        }
+    }
 }
