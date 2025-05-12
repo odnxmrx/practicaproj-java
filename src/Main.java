e0dca3ffb1b309fdf548a5ce6a19e6f856cf0ff3
odnxmrx <armandopinedag@outlook.com>
2025-05-11 22:06:51 -0600

    first commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    /* Al agregar la palabra 'static', indicamos que el método main es un método de clase
+    y no de instancia, lo que permite que la máquina virtual de Java pueda ejecutar el
+    programa sin necesidad de crear una instancia de la clase.
+     */
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.print("Hello and welcome!");
+
+        for (int i = 1; i <= 5; i++) {
+            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+            System.out.println("i = " + i);
+        }
+    }
+}
