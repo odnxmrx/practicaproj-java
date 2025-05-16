d1d68e6d52e485e28baddc04a144d5e3cc3b60bb
odnxmrx <armandopinedag@outlook.com>
2025-05-15 22:28:34 -0600

    create podcast classes

---
+++ myradio/src/models/Podcast.java
@@ -0,0 +1,22 @@
+package models;
+
+public class Podcast extends Audio {
+    private String presenter;
+    private String description;
+
+    public String getPresenter() {
+        return presenter;
+    }
+
+    public void setPresenter(String presenter) {
+        this.presenter = presenter;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
